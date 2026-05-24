ba573f2dea708f8fd2df1e69f2c7ea8f348907d1
shreya-oeo <138018355+shreya-oeo@users.noreply.github.com>
2026-05-24 12:58:56 +0300

    bbox crop unwanted white space

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ def analyze(path,h=16,b=16):
     #img = Image.open("./characters/D.png").convert("RGBA")
     #img = img.convert('L')
     img = load_grayscale(path)
+    bbox = img.getbbox()
+    img = img.crop(bbox)
     img = img.resize((h,b))
     img.save("output.png")
     pixels = list(img.getdata())
